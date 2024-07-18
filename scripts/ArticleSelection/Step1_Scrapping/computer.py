f9b9e5d475e6f10b32e654a9016c01fdf1d6f360
Hugo <gogo.bourreau@hotmail.fr>
2024-07-18 11:03:19 +0200

    stage

--- scripts/ArticleSelection/Step1_Scrapping/computer.py
+++ scripts/ArticleSelection/Step1_Scrapping/computer.py
@@ -13,10 +13,12 @@ from selenium.webdriver.chrome.options import Options
 def scrap_computer(request, path):
     print("COMPUTER STRATING SCRAPING")
     options = Options()
-    options.add_argument("--window-size=1280,720")
+    options.add_argument("--window-size=1920,1080")
     options.add_argument("--start-maximized")
     options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
 
